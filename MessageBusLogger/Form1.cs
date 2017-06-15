2eb5ac5b2eb93dbdc85c49d394f75e83a9ba1928
FOURTH\dkulezich <denitsa.kulezich@fourth.com>
2017-06-15 17:34:07 +0300

    Added logic for subscribing to all topics

--- MessageBusLogger/Form1.cs
+++ MessageBusLogger/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Azure;
 using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,16 +15,28 @@ namespace MessageBusLogger
 {
     public partial class Form1 : Form
     {
+        private const string SUBSCRIPTION_NAME = "messageBusLogger_subscr";
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void subscrBtn_Click(object sender, EventArgs e)
+        private async void subscrBtn_Click(object sender, EventArgs e)
         {
             string connectionString = CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString");
 
             var namespaceManager = NamespaceManager.CreateFromConnectionString(connectionString);
+
+            var topicList = await namespaceManager.GetTopicsAsync();
+
+            foreach (TopicDescription topic in topicList)
+            {
+                if(!namespaceManager.SubscriptionExists(topic.Path, SUBSCRIPTION_NAME))
+                {
+                    await namespaceManager.CreateSubscriptionAsync(topic.Path, SUBSCRIPTION_NAME);
+                }
+                
+            }
         }
     }
 }
