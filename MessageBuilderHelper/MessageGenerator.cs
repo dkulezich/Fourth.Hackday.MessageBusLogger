2d7f80fb588d0a6ba1cf591e85d95a9b3f0bbcd8
Scorp5 <ilian_oven@abv.bg>
2017-06-19 21:20:59 +0300

    add fourth orchestration and console helper

---
+++ MessageBuilderHelper/MessageGenerator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Fourth.Orchestration.Model.ProductCatalogue;
+using Common = Fourth.Orchestration.Model.Common;
+using System.Collections;
+using System.Collections.Generic;
+using Fourth.Orchestration.Storage.Azure;
+using Fourth.Orchestration.Messaging.Azure;
+using Fourth.Orchestration.Messaging;
+
+namespace MessageBuilderHelper
+{
+    public class MessageGenerator
+    {
+        /*
+         15200 - Leicester
+         15298 - Barry Pub789
+         16202 - TestingNotCurrency
+         16199 - 11 test
+        */
+        private IList<long> locationIds = new List<long> { 15200, 15298, 16202 }; // 16199 - used by the mobile team
+        private IList<long> productIds = new List<long> { 12345, 12157, 31892, 31893, 31894, 31895, 31896 };
+        private Random randomGenerator;
+
+        public MessageGenerator()
+        {
+            this.randomGenerator = new Random();
+        }
+
+        public Events.ProductLocationsModified CreateProductLocationsModified()
+        {
+            var newProductLocationBuilder = Events.ProductLocationsModified.CreateBuilder();
+            newProductLocationBuilder.SetSequenceNumber(Fourth.Orchestration.Model.SequenceNumbers.GetNext());
+            newProductLocationBuilder.SetCustomerCanonicalId("0018E00000UGGbqQAH");
+            newProductLocationBuilder.SetSource(Events.SourceSystem.R9);
+            newProductLocationBuilder.SetProductExternalId(productIds[this.randomGenerator.Next(productIds.Count)]);
+            newProductLocationBuilder.SetProductExternalClass(Events.ProductClass.PRODUCT_ITEM);
+
+            var newLocationAssignment = Events.ProductLocationsModified.Types.LocationAssignment.CreateBuilder();
+            newLocationAssignment.SetLocationExternalId(locationIds[this.randomGenerator.Next(locationIds.Count)]);
+
+            var parLevel = Common.Decimal.BuildFromDecimal(decimal.Parse(this.randomGenerator.Next(10).ToString()));
+            newLocationAssignment.SetParLevel(parLevel);
+
+            newProductLocationBuilder.AddLocationAssignments(newLocationAssignment.Build());
+
+            return newProductLocationBuilder.Build();
+        }
+    }
+}
---
+++ MessageBuilderHelper/Program.cs
@@ -0,0 +1,30 @@
+﻿using Fourth.Orchestration.Messaging.Azure;
+using Fourth.Orchestration.Storage.Azure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageBuilderHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var messageStore = new AzureMessageStore();
+            var messageFactory = new AzureMessagingFactory(messageStore);
+            var messageBus = messageFactory.CreateMessageBus();
+            var messageGenerator = new MessageGenerator();
+
+            for (int i = 0; i < 50; i++)
+            {
+                var message = messageGenerator.CreateProductLocationsModified();
+                messageBus.Publish(message);
+                Console.WriteLine("{0} - {1}", i + 1, message.ToString());
+                Console.WriteLine();
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
