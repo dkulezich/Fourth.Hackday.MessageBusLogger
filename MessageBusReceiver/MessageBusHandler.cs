8a0b7bca0944eee6dfb591b73659fafab7ed1da0
Scorp5 <ilian_oven@abv.bg>
2017-06-24 22:28:12 +0300

    add a handler and a listener classes

---
+++ MessageBusReceiver/MessageBusHandler.cs
@@ -0,0 +1,14 @@
+﻿using Fourth.Orchestration.Messaging;
+using Google.ProtocolBuffers;
+using System.Threading.Tasks;
+
+namespace MessageBusReceiver
+{
+    public class MessageHandler<T> : IMessageHandler<T> where T : IMessage
+    {
+        public Task<MessageHandlerResult> HandleAsync(T payload, string trackingId)
+        {
+            return Task.FromResult<MessageHandlerResult>(MessageHandlerResult.Success);
+        }
+    }
+}
---
+++ MessageBusReceiver/MessageEventListener.cs
@@ -0,0 +1,34 @@
+﻿using Fourth.Orchestration.Messaging;
+using Fourth.Orchestration.Messaging.Azure;
+using Fourth.Orchestration.Storage;
+using Fourth.Orchestration.Storage.Azure;
+using Fourth.Orchestration.Model.ProductCatalogue;
+
+namespace MessageBusReceiver
+{
+    public class MessageEventListener
+    {
+        private IMessageStore messageStore;
+        private IMessagingFactory messageFactory;
+        private IMessageListener messageListener;
+
+        public MessageEventListener(string subscription)
+        {
+            messageStore = new AzureMessageStore();
+            messageFactory = new AzureMessagingFactory(messageStore);
+            messageListener = messageFactory.CreateMessageListener("Message Bus Logger");
+        }
+
+        public void StartListen()
+        {
+            messageListener.RegisterHandler(new MessageHandler<Events.ProductLocationsModified>());
+            messageListener.StartListener();
+        }
+
+        public void StopListen()
+        {
+            messageListener.StopListener();
+            messageListener.Dispose();
+        }
+    }
+}
