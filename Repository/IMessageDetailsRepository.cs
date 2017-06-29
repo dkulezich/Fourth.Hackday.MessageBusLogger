a62233ab7230902d3d21eef7b8af4c50b7716c5e
dkulezich <denikul@hotmail.com>
2017-06-29 16:37:00 +0300

    Adding reposotory project

---
+++ Repository/IMessageDetailsRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Repository
+{
+    public interface IMessageDetailsRepository<T> : IRepository<T>
+    {
+        T GetByType(string messageType);
+        T GetBySystem(string sourceSystem);
+        T GetByEnvironment(string environment);
+        T GetByDate(DateTime date);
+    }
+}
---
+++ Repository/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Repository
+{
+    public interface IRepository<T>
+    {
+        void Insert(T entity);
+        void Delete(T entity);
+        T GetById(int id);
+        IList<T> GetAll();
+    }
+}
