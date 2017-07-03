73828e144104d6f7465f23d2d1815b11f7b01549
Deni <Deni@NEO>
2017-07-03 22:16:11 +0300

    Added getting the message's source system

--- MessageBusReceiver/MessageBusHandler.cs
+++ MessageBusReceiver/MessageBusHandler.cs
@@ -2,6 +2,7 @@
 using DbCreator.Model;
 using Fourth.Orchestration.Messaging;
 using Google.ProtocolBuffers;
+using Newtonsoft.Json.Linq;
 using Repository;
 using System;
 using System.Threading.Tasks;
@@ -19,16 +20,20 @@ namespace MessageBusReceiver
 
         public Task<MessageHandlerResult> HandleAsync(T payload, string trackingId)
         {
+            var jObject = JObject.Parse(payload.ToJson());
+            var jToken = jObject.SelectToken("$.Source");
+
             var messageDetails = new MessageDetails();
             messageDetails.Date = DateTime.UtcNow;
             messageDetails.Environment = "qai";
             messageDetails.Type = payload.GetType().FullName;
             messageDetails.TrackingId = trackingId;
+            messageDetails.SourceSystem = jToken.ToString();
             messageContent.Message = payload.ToByteString().ToBase64();
             messageDetails.MessageContent = messageContent;
             messageContentRepository.Insert(messageDetails);
 
             return Task.FromResult<MessageHandlerResult>(MessageHandlerResult.Success);
-        }
+        }        
     }
 }
