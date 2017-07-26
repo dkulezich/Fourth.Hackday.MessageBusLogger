be6499190015c7016b67d2c0f5ab5fe9ff4d19f5
ilivanov <ilivanov@SOF-7040-027>
2017-07-26 12:02:25 +0300

    ui changes

--- MessageBusLogger_WPF/MainWindow.xaml.cs
+++ MessageBusLogger_WPF/MainWindow.xaml.cs
@@ -296,7 +296,7 @@ namespace MessageBusLogger_WPF
                         var match = reg.Match(start.GetTextInRun(LogicalDirection.Forward));
 
                         var textrange = new TextRange(start.GetPositionAtOffset(match.Index, LogicalDirection.Forward), start.GetPositionAtOffset(match.Index + match.Length, LogicalDirection.Backward));
-                        textrange.ApplyPropertyValue(TextElement.ForegroundProperty, new SolidColorBrush(Colors.Cyan));
+                        textrange.ApplyPropertyValue(TextElement.ForegroundProperty, new SolidColorBrush(Colors.Red));
                         textrange.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Bold);
                         start = textrange.End; 
                     }
