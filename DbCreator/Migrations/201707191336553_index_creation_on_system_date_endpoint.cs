7b9ea7213f122415f30cfb502701b041e97fd4f4
Denitsa Kulezich <Denitsa.Kulezich@fourth.com>
2018-10-18 14:33:22 +0300

    packages update

--- DbCreator/Migrations/201707191336553_index_creation_on_system_date_endpoint.cs
+++ DbCreator/Migrations/201707191336553_index_creation_on_system_date_endpoint.cs
@@ -10,12 +10,10 @@ namespace DbCreator.Migrations
             AlterColumn("dbo.MessageDetails", "MessageBusEndpoint", c => c.String(nullable: false, maxLength: 400));
             CreateIndex("dbo.MessageDetails", "SourceSystem");
             CreateIndex("dbo.MessageDetails", "Date");
-            CreateIndex("dbo.MessageDetails", "MessageBusEndpoint");
         }
         
         public override void Down()
         {
-            DropIndex("dbo.MessageDetails", new[] { "MessageBusEndpoint" });
             DropIndex("dbo.MessageDetails", new[] { "Date" });
             DropIndex("dbo.MessageDetails", new[] { "SourceSystem" });
             AlterColumn("dbo.MessageDetails", "MessageBusEndpoint", c => c.String(nullable: false));
